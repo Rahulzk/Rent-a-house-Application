cbe2069386b3a3a4bc6a9ccb5571df57c04d06e5
Rahul kumar <rahulpatel14042002@gmail.com>
2022-12-05 11:12:34 +0530

    Title changed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ import Typography from '@mui/material/Typography';
 const Header = () => {
   return (
     <div style={{height:50, display:"flex",justifyContent:'space-between',alignItems:'center',backgroundColor:'#d3e3d7',}} >
-        <Typography style={{marginLeft:10,fontFamily:'Roboto Condensed',fontWeight:'bold'}}>HouseRentApp</Typography>
+        <Typography style={{marginLeft:10,fontFamily:'Roboto Condensed',fontWeight:'bold'}}>HouseRentingApp</Typography>
         <div>
             <Button variant="contained" style={{marginRight:10}}>Login</Button>
             <Button variant="outlined" style={{marginRight:10}}>Sign up</Button>
